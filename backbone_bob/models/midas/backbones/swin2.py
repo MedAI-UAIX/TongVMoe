5cb14a17c3bfd8723c48951497b298d5ae1b01ff
xixiheiheixixi <140492955+xixiheiheixixi@users.noreply.github.com>
2025-06-13 16:15:37 +0800

    Add files via upload

---
+++ backbone_bob/models/midas/backbones/swin2.py
@@ -0,0 +1,38 @@
+import timm
+
+from .swin_common import _make_swin_backbone
+
+
+def _make_pretrained_swin2l24_384(pretrained, hooks=None):
+    model = timm.create_model("swinv2_large_window12to24_192to384_22kft1k", pretrained=pretrained)
+
+    hooks = [1, 1, 17, 1] if hooks == None else hooks
+    return _make_swin_backbone(
+        model,
+        hooks=hooks
+    )
+
+
+def _make_pretrained_swin2b24_384(pretrained, hooks=None):
+    import torch
+    model = timm.create_model("swinv2_base_window12to24_192to384_22kft1k", pretrained=False)
+    # if pretrained:
+    #     checkpoints = torch.load("/home/user02/LiuSH/01tongue/工学/新mtl组合-tongue/权重文件夹/pytorch_model.bin")
+    #     model.load_state_dict(checkpoints)
+
+    hooks = [1, 1, 17, 1] if hooks == None else hooks
+    return _make_swin_backbone(
+        model,
+        hooks=hooks
+    )
+
+
+def _make_pretrained_swin2t16_256(pretrained, hooks=None):
+    model = timm.create_model("swinv2_tiny_window16_256", pretrained=pretrained)
+
+    hooks = [1, 1, 5, 1] if hooks == None else hooks
+    return _make_swin_backbone(
+        model,
+        hooks=hooks,
+        patch_grid=[64, 64]
+    )
